b5276df1606a51bf89e9fb335c5b65e9d3a1a1b0
bobtrolledus <335730800@gapps.yrdsb.ca>
2022-06-08 14:15:42 -0400

    created title screen class and image

---
+++ titleScreen.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class titleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class titleScreen extends World
+{
+    private int transVal;
+    /**
+     * Constructor for objects of class titleScreen.
+     * 
+     */
+    public titleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1000, 800, 1); 
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+               
+        }
+    }
+}
